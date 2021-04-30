6120fe7850c0763262185ed67bd778d224d3735c
Davide Carini <dcarini@techsol.it>
2021-04-30 21:42:59 +0200

    basic form

---
+++ TextManipulator/Controllers/TextManipulatorsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TextManipulator.ViewModel;
+
+namespace TextManipulator.Controllers
+{
+    public class TextManipulatorsController : Controller
+    {
+        ITextManipulatorAlgorithm _textManipulator;
+        public TextManipulatorsController()
+        {
+            _textManipulator = new LargestWordFinder();
+        }
+        
+        public ActionResult Index()
+        {
+            var viewModel = new TextManipulatorFormViewModel();
+            viewModel.AlgorithmName = _textManipulator.AlgorithmName;
+
+            return View("Index", viewModel);
+        }
+
+        public ActionResult Manipulate(string text)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var manipulatedText = _textManipulator.ManipulateText(text);
+            TextManipulatorFormViewModel viewModel = new TextManipulatorFormViewModel()
+            {
+                AlgorithmName = _textManipulator.AlgorithmName,
+                Text = text,
+                ManipulatedText = manipulatedText
+            };
+
+            return View("Index", viewModel);
+        }
+    }
+}
---
+++ TextManipulator/ViewModel/TextManipulatorFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TextManipulator.ViewModel
+{
+    public class TextManipulatorFormViewModel
+    {
+        [Required]
+        [Display (Name = "Type your text:")]
+        public string Text { get; set; }
+
+        public string ManipulatedText { get; set; }
+
+        public string AlgorithmName { get; set; }
+
+        //public IEnumerable<string> Algorithms { get; set; }
+        //[Required]
+        //public string SelectedAlgorithm { get; set; }
+    }
+}
