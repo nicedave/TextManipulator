c547c3e723ea2bd1e588f76ff192a19732d76e9c
Davide Carini <dcarini@techsol.it>
2021-05-02 12:42:04 +0200

    Aggiunti test controller mvc

---
+++ TextManipulator.Tests/Controllers/TextManipulatorsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using NUnit.Framework;
+using TextManipulator.Controllers;
+using TextManipulator.Controllers.Algorithms;
+using TextManipulator.ViewModels;
+
+namespace TextManipulator.Tests.Controllers
+{
+    [TestFixture]
+    public class TextManipulatorsControllerTests
+    {
+        Mock<IAlgorithmsManager> _manager;
+        TextManipulatorsController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _manager = new Mock<IAlgorithmsManager>();
+            _controller = new TextManipulatorsController(_manager.Object);
+        }
+
+        [Test]
+        public void Index_WhenCalled_GetsAvailableAlgorithms() 
+        {
+            var result = _controller.Index();
+
+            _manager.Verify(m => m.GetAvailableAlgorithms());
+        }
+
+        [Test]
+        public void Manipulate_ViewModelIsValid_InvokesExecuteAlghoritms()
+        {
+            var viewmodel = new TextManipulatorFormViewModel() 
+            {
+                Text = "text",
+                SelectedAlgorithm = "algo"
+            };
+
+            var result = _controller.Manipulate(viewmodel);
+
+            _manager.Verify(m => m.ExecuteAlgorithm(viewmodel.SelectedAlgorithm, viewmodel.Text));
+        }
+    }
+}
